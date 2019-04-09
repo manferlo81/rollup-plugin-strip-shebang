1cec749878b2471b511fac4b94e19ceed53659f4
Manuel Fernandez <manferlo81@gmail.com>
2019-04-09 14:16:54 +0700

    removed own rollup-pluginutils type declaration

--- declarations/rollup-pluginutils.d.ts
+++
@@ -1,9 +0,0 @@
-// rollup-pluginutils desn't have a type declaration file
-
-declare type MinimatchPattern = Array<string | RegExp> | string | RegExp | null;
-declare type Filter = (id: any) => boolean;
-
-export function createFilter(
-  include?: MinimatchPattern,
-  exclude?: MinimatchPattern,
-): Filter;
