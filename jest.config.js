f7c2f3764d5f8224a98f6b720ddaf5c49a89b521
Manuel Fernandez <manferlo81@gmail.com>
2019-06-07 04:53:45 +0700

    jest config improvements

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,22 @@
+const { main } = require('./package.json')
 const CI = !!process.env.CI;
 
 module.exports = {
 
   testEnvironment: "node",
+  browser: false,
+
+  cacheDirectory: ".cache/jest",
 
   collectCoverage: true,
+  collectCoverageFrom: [
+    main,
+  ],
   coverageDirectory: "coverage",
   coverageReporters: [
     CI ? "json" : "lcov",
     "text",
     "text-summary",
-  ],
-  collectCoverageFrom: [
-    "dist/**",
   ],
 
   verbose: true,
