89e95a0d19a3da0996f6804e6f175dbb89b18fcb
Manuel Fernandez <manferlo81@gmail.com>
2019-06-30 11:48:17 +0700

    edited test

--- test/skip.test.ts
+++ test/skip.test.ts
@@ -2,19 +2,19 @@ import generate from "./tools/generate";
 
 describe("skip", () => {
 
-  test("skip files without shebang", async () => {
+  test("should skip files without shebang", async () => {
 
     await generate("example2.js");
 
   });
 
-  test("skip files using include", async () => {
+  test("should skip files using include", async () => {
 
     await generate("example2.js", { include: /anyfile\.js/ });
 
   });
 
-  test("skip files using exclude", async () => {
+  test("should skip files using exclude", async () => {
 
     await generate("example2.js", { exclude: /example2\.js/ });
 
