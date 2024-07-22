38931c1ba5b4037d019170b1595a2560f76bd60c
Manuel Fernández <manferlo81@gmail.com>
2024-07-22 12:15:03 +0700

    chore(deps): Update bundlib to v0.19.0

---
+++ bundlib.config.ts
@@ -0,0 +1,10 @@
+import { BundlibConfig } from 'bundlib';
+
+const config: BundlibConfig = {
+  esModule: true,
+  interop: true,
+  equals: true,
+  project: 'tsconfig-build.json',
+};
+
+export default config;
